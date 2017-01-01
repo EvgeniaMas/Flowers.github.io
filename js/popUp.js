eae6a40265cff5fd8ebd65dbd0245513ac14741d
Евгения Масликова < >
2017-01-01 15:19:02 +0300

    add

---
+++ js/popUp.js
@@ -0,0 +1,26 @@
+
+   $(document).ready(function(){
+         
+        PopUpHide();
+    });
+
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+    }
+    //Функция скрытия PopUp1
+    function PopUpHide(){
+        $("#popup1").hide();
+    }
+
+
+    function PopUpShow2(){
+        $("#popup2").show();
+    }
+    //Функция скрытия PopUp1
+    function PopUpHide2(){
+        $("#popup2").hide();
+    }
+
+
+
